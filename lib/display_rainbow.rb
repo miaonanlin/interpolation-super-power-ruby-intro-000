82efe0020bb20cbf62ed1f4784c48be615f47071
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-03 19:12:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+def display_rainbow(c)
+  puts "R: #{c[0]}, O: #{c[1]}, Y: #{c[2]}, G: #{c[3]}, B: #{c[4]}, I: #{c[5]}, V: #{c[6]}"
 end
